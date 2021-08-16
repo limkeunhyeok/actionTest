279ed464b43af960da293f6843cbf3371edaa231
limkeunhyeok <gorloom6425@naver.com>
2021-08-16 22:02:07 +0900

    test11

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    res.send("Hello world222222");
 });
 
 app.get("/action", (req, res) => {
-    res.send("hello world!");
+    res.send("hello world!2222222");
 });
 
 app.get("/test", (req, res) => {
-    res.send("test!");
+    res.send("test!222222");
 });
 
 app.listen(4000, () => {
